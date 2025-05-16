e90619da8515b3eb176f6024c7970e78ca20171c
3m <3m@gmail.com>
2025-05-17 00:38:41 +0300

    edit NavBar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,33 @@
 function Navbar() {
   return (
-    <div className="navbar bg-primary text-primary-content shadow-md">
-      <div className="container mx-auto">
-        <div className="flex-1">
-          <a href="/" className="text-xl font-bold">Image Description AI</a>
-        </div>
-        <div className="flex-none">
-          <ul className="menu menu-horizontal px-1">
+    <div className="navbar bg-base-100 shadow-sm">
+  <div className="navbar-start">
+    <div className="dropdown">
+      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+        <svg xmlns="http://www.w3.org/2000/svg" className="  h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+      </div>
+      <ul
+        tabIndex={0}
+        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
             <li><a href="/">Home</a></li>
             <li><a href="/about">About</a></li>
             <li><a href="/help">Help</a></li>
-          </ul>
-        </div>
-      </div>
+      </ul>
     </div>
+    <div>
+    <a href="/" className="btn btn-ghost text-xl">CaptionCraft</a>
+    </div>
+  </div>
+  <div className="navbar-center hidden lg:flex">
+    <ul className="menu menu-horizontal px-1">
+            <li><a href="/">Home</a></li>
+            <li><a href="/about">About</a></li>
+            <li><a href="/help">Help</a></li>
+    </ul>
+  </div>
+
+</div>
+
   );
 }
 
